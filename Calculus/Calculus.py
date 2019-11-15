07627b17699450afb17de775e070554460226ab0
Ian Diaz <idiaz@itba.edu.ar>
2019-11-14 21:42:49 -0300

    sd

--- Calculus/Calculus.py
+++ Calculus/Calculus.py
@@ -15,14 +15,62 @@ def exportToSpice(file, cellnumber,values):
     return
 
 ExportarLTSpice = True
-Montecarlo = False
+Montecarlo = True
 
-data1 = FleicherTow.FTLow(   0.050699, 2*3.142*9715.92 ,1.41, 1e3,1e3,1e3,10e-9,10e-9)
-data2 = FleicherTow.FTHigh(  0.88511 , 2*3.142*1953.46 ,4.44, 1e3,1e3,1e3,10e-9,10e-9)
-data3 = FleicherTow.FTBand(  5.41377 , 2*3.142*2577.97 ,1.41, 1e3,1e3,1e3,10e-9,10e-9)
-data4 = sallenLow.calcValues(33.075,   2*3.142*5004.73 ,0.93, 10e3,1e3,10e-9)
-data5 = FleicherTow.FTHigh(  1.1776 ,  2*3.142*12822   ,4.44, 1e3,1e3,1e3,10e-9,10e-9)
+G1=-5.19
+Q1=4.44
+fo1 =12822.6
+type1 = 'hp'
 
+G2=-2.71
+Q2=1.41
+fo2 =9714.95
+type2 = 'lp'
+
+
+G3=-0.23
+Q3=4.44
+fo3 =1949.6
+type3 = 'hp'
+
+
+G4=24.61
+Q4=0.93
+fo4 =4999.9
+type4 = 'lp'
+
+
+G5=3.08
+Q5=1.41
+fo5 =2573.25
+type5 = 'bp'
+
+
+
+k1 = 10**(G1/20)
+w1 = 2*3.14159*fo1
+k2 = 10**(G2/20)
+w2 = 2*3.14159*fo2
+k3 = 10**(G3/20)
+w3 = 2*3.14159*fo3
+k4 = 10**(G4/20)
+w4 = 2*3.14159*fo4
+k5 = 10**(G5/20)
+w5 = 2*3.14159*fo5
+
+k = [k1,k2,k3,k4,k5]
+w = [w1,w2,w3,w4,w5]
+Q = [Q1,Q2,Q3,Q4,Q5]
+typef = [type1,type2,type3,type4,type5]
+data = []
+
+for i in range(0,len(k)):
+    if typef[i] == 'lp':
+        data.append(FleicherTow.FTLow(k[i],w[i],Q[i],1e3,1e3,1e3,10e-9,10e-9))
+    elif typef[i] == 'hp':
+        data.append(FleicherTow.FTHigh(k[i],w[i],Q[i],1e3,1e3,1e3,10e-9,10e-9))
+    elif typef[i] == 'bp':
+        data.append(FleicherTow.FTBand(k[i],w[i],Q[i],1e3,1e3,1e3,10e-9,10e-9))
 
 if ExportarLTSpice is True:
     timestr = time.strftime("%Y%m%d-%H%M%S")
@@ -32,10 +80,8 @@ if ExportarLTSpice is True:
         inF = open("simulation_template2.asc", "r")
     outF=open("simulation_generated_"+timestr+".asc","w+")
     outF.write(inF.read())
-    exportToSpice(outF,1,data1)
-    exportToSpice(outF,2,data2)
-    exportToSpice(outF,3,data3)
-    exportToSpice(outF,4,data4)
-    exportToSpice(outF,5,data5)
+    for i in range(0,len(data)):
+        exportToSpice(outF,i+1,data[i])
     outF.close()
     inF.close()
+print(typef)
--- Calculus/FleicherTow.py
+++ Calculus/FleicherTow.py
@@ -1,23 +1,35 @@
+from eseries import find_nearest, E12,E24,E48
+
 
 def FTHigh(k,wo,Q,R8,R3,R7,C1,C2):
     R2 = R8 / (wo**2 * R3*R7*C1*C2)
+    R2 = find_nearest(E24,R2)
     R1 = Q/(C1*wo)
+    R1 = find_nearest(E24,R1)
     R6 = R8/k#-R8/k
+    R6 = find_nearest(E24,R6)
     R4 = R1 * R6/R7
+    R4 = find_nearest(E24,R4)
     temp = {'R1':R1,'R2':R2, 'R3':R3, 'R4':R4, 'R6':R6, 'R7':R7, 'R8':R8, 'C1':C1, 'C2':C2}
     return temp
 
 def FTBand(k,wo,Q,R8,R3,R7,C1,C2):
     R2 = R8 / (wo**2 * R3*R7*C1*C2)
+    R2 = find_nearest(E24,R2)
     R1 = Q/(C1*wo)
+    R1 = find_nearest(E24,R1)
     R4 = R1*R8/(k*R7)
+    R4 = find_nearest(E24,R4)
     temp = {'R1':R1,'R2':R2, 'R3':R3, 'R4':R4, 'R7':R7, 'R8':R8, 'C1':C1, 'C2':C2}
     return temp
 
 def FTLow(k,wo,Q,R8,R3,R7,C1,C2):
     R2 = R8 / (wo**2 * R3*R7*C1*C2)
+    R2 = find_nearest(E24,R2)
     R1 = Q/(C1*wo)
+    R1 = find_nearest(E24,R1)
     R5 = R2/k#-R2/k 
+    R5 = find_nearest(E24,R5)
     temp = {'R1':R1,'R2':R2, 'R3':R3,'R5':R5, 'R7':R7, 'R8':R8, 'C1':C1, 'C2':C2}
     return temp
     
